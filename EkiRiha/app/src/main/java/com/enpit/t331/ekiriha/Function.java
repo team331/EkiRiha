ee0567d0993aebd2fedfe4bea12f7f286cfe9799
sixleaveakkm <sixleaveakkm@gmail.com>
2016-08-29 14:46:59 +0900

    add maps activity and graph calculate

--- EkiRiha/app/src/main/java/com/enpit/t331/ekiriha/Function.java
+++ EkiRiha/app/src/main/java/com/enpit/t331/ekiriha/Function.java
@@ -26,30 +26,4 @@ public class Function {
             Toast.makeText(ct, str , Toast.LENGTH_SHORT).show();
     }
 
-
-    public void CalLinkList(Context context, int station_pos , int start_id , int end_id){
-        String station = context.getResources().getStringArray(R.array.station_mark)[station_pos];
-        String station_select_map = station + "_select_map";
-        int identify = context.getResources().getIdentifier(station_select_map, "array", context.getPackageName());
-
-        String[] mItems = context.getResources().getStringArray(identify);
-        int start_map_id = Integer.parseInt(mItems[start_id]);
-        int end_map_id = Integer.parseInt(mItems[end_id]);
-
-        String station_links = station + "_links";
-        identify = context.getResources().getIdentifier(station_links, "array", context.getPackageName());
-        String[] links = context.getResources().getStringArray(identify);
-
-        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
-
-        for(String link : links) {
-            String[] link_pair_s = link.split(",");
-            int[] link_pair = new int[2];
-            link_pair[0] = Integer.parseInt(link_pair_s[0]);
-            link_pair[1] = Integer.parseInt(link_pair_s[1]);
-            map.put(link_pair[0], link_pair[1]);
-            map.put(link_pair[1], link_pair[0]);
-        }
-    }
-
 }
