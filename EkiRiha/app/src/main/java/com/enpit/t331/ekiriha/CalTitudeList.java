2047b5ecf0eac75850471784a216e422e3241631
sixleaveakkm <sixleaveakkm@gmail.com>
2016-08-29 10:34:28 +0900

    remake android project using t331

---
+++ EkiRiha/app/src/main/java/com/enpit/t331/ekiriha/CalTitudeList.java
@@ -0,0 +1,13 @@
+package com.enpit.t331.ekiriha;
+
+/**
+ * Created by woo on 16/8/26.
+ */
+public class CalTitudeList {
+/*todo
+    station id -> station_name -> station_select ->station_select_map
+    start end -> station_select_map
+    [station]_links -> link
+    link -> [station_points]
+ */
+}
--- EkiRiha/app/src/main/java/com/enpit/t331/ekiriha/Function.java
+++ EkiRiha/app/src/main/java/com/enpit/t331/ekiriha/Function.java
@@ -1,7 +1,13 @@
 package com.enpit.t331.ekiriha;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.widget.Toast;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by woo on 16/8/26.
@@ -18,6 +24,32 @@ public class Function {
         String str = ct.getString(text);
         if(str != null)
             Toast.makeText(ct, str , Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void CalLinkList(Context context, int station_pos , int start_id , int end_id){
+        String station = context.getResources().getStringArray(R.array.station_mark)[station_pos];
+        String station_select_map = station + "_select_map";
+        int identify = context.getResources().getIdentifier(station_select_map, "array", context.getPackageName());
+
+        String[] mItems = context.getResources().getStringArray(identify);
+        int start_map_id = Integer.parseInt(mItems[start_id]);
+        int end_map_id = Integer.parseInt(mItems[end_id]);
+
+        String station_links = station + "_links";
+        identify = context.getResources().getIdentifier(station_links, "array", context.getPackageName());
+        String[] links = context.getResources().getStringArray(identify);
+
+        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+
+        for(String link : links) {
+            String[] link_pair_s = link.split(",");
+            int[] link_pair = new int[2];
+            link_pair[0] = Integer.parseInt(link_pair_s[0]);
+            link_pair[1] = Integer.parseInt(link_pair_s[1]);
+            map.put(link_pair[0], link_pair[1]);
+            map.put(link_pair[1], link_pair[0]);
+        }
     }
 
 }
