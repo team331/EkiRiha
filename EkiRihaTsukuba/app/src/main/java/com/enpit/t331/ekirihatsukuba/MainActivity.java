2e388c0c145bea3e77ed44321dbf323245cb3ab4
sixleaveakkm <sixleaveakkm@gmail.com>
2016-10-17 20:04:31 +0900

    Tsukubaサンプル

---
+++ EkiRihaTsukuba/app/src/main/java/com/enpit/t331/ekirihatsukuba/MainActivity.java
@@ -0,0 +1,45 @@
+package com.enpit.t331.ekirihatsukuba;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//起動スクリン及びユーザーデータを読む
+public class MainActivity extends AppCompatActivity {
+    private Context m_context = this;
+    private static final int MIN_SHOW_TIME = 800;//ms
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new AsyncTask<Void, Void, Integer>(){
+
+            @Override
+            protected Integer doInBackground(Void... params){
+                int result=0;
+                long startTime = System.currentTimeMillis();
+                //todo: load data
+                long loadingTime = System.currentTimeMillis() - startTime;
+                if(loadingTime < MIN_SHOW_TIME){
+                    try{
+                        Thread.sleep(MIN_SHOW_TIME - loadingTime);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(Integer result){
+                Intent it = new Intent();
+                it.setClass(m_context, MapsActivity.class);
+                startActivity(it);
+            };
+        }.execute(new Void[]{});
+    }
+}
